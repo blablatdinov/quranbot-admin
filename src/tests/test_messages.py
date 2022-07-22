2ea6354c5d124ca3ed86d7cf06e4d938335de145
blablatdinov <45946541+blablatdinov@users.noreply.github.com>
2022-07-22 11:55:01 +0300

    create message operations plugs (#3)

--- src/tests/test_messages.py
+++ src/tests/test_messages.py
@@ -5,7 +5,47 @@ from main import app
 client = TestClient(app)
 
 
-def test():
+def test_get_list():
     got = client.get('/api/v1/messages')
 
     assert got.status_code == 200
+    assert got.json() == {
+        'count': 2,
+        'next': '/api/v1/messages/?page=2',
+        'prev': '/api/v1/messages/?page=0',
+        'results': [
+            {
+                'id': 1,
+                'message_id': 1,
+                'message_source': 'from 23343',
+                'sending_date': '1000-01-01T00:00:00',
+                'text': 'Hello...',
+            },
+            {
+                'id': 2,
+                'message_id': 10,
+                'message_source': 'Mailing (45)',
+                'sending_date': '1000-01-01T00:00:00',
+                'text': 'Bye...',
+            },
+        ],
+    }
+
+
+def test_get_message():
+    got = client.get('/api/v1/messages/34')
+
+    assert got.status_code == 200
+    assert got.json() == {
+        'id': 34,
+        'message_id': 1,
+        'message_source': 'from 23343',
+        'sending_date': '1000-01-01T00:00:00',
+        'text': 'Hello...',
+    }
+
+
+def test_delete_message_from_telegram():
+    got = client.delete('/api/v1/messages/34/delete-from-chat')
+
+    assert got.status_code == 204
