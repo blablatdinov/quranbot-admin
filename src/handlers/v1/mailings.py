cdb77858b6239b4164505e7b739dbc899def6ed7
blablatdinov <45946541+blablatdinov@users.noreply.github.com>
2022-07-22 12:29:15 +0300

    Mailings routers plug (#4)

---
+++ src/handlers/v1/mailings.py
@@ -0,0 +1,50 @@
+from fastapi import APIRouter, status
+from pydantic import BaseModel
+
+router = APIRouter(prefix='/mailings')
+
+
+class MailingModel(BaseModel):
+    """Модель рассылки."""
+
+    id: int
+
+
+class MailingCreateModel(BaseModel):
+    """Модель для создания рассылок."""
+
+    text: str
+
+
+class MailingCreateResponseModel(MailingCreateModel):
+    """Модель ответа запроса создания рассылок."""
+
+    id: int
+
+
+@router.get('/', status_code=status.HTTP_200_OK, response_model=list[MailingModel])
+def get_mailings():
+    """Получить список рассылок.
+
+    :return: list[MailingModel]
+    """
+    return [MailingModel(id=1)]
+
+
+@router.delete('/{mailing_id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_mailing_from_telegram(mailing_id: int):
+    """Удаление сообщений, входящих в рассылку.
+
+    :param mailing_id: int
+    """
+    return  # noqa: WPS324
+
+
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=MailingCreateResponseModel)
+def create_mailing_from_telegram(input_data: MailingCreateModel) -> MailingCreateResponseModel:
+    """Создание рассылки.
+
+    :param input_data: MailingCreateModel
+    :return: MailingCreateResponseModel
+    """
+    return MailingCreateResponseModel(id=1, text=input_data.text)
