b17481c86343caadca02e858adbc0528be0de91f
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-07-23 10:42:16 +0300

    lint

--- src/conftest.py
+++ src/conftest.py
@@ -3,9 +3,8 @@ import pytest
 from fastapi.testclient import TestClient
 
 from db import db_connection
-from settings import settings
-
 from main import app
+from settings import settings
 
 
 @pytest.fixture()
@@ -14,22 +13,22 @@ def client():
 
 
 @pytest.fixture()
-async def create_db():
+async def create_db():  # noqa: WPS217
     base_connection = await asyncpg.connect(settings.DATABASE_URL)
-    await base_connection.execute("""DROP DATABASE IF EXISTS qbot_test""")
-    await base_connection.execute("""CREATE DATABASE qbot_test""")
+    await base_connection.execute('DROP DATABASE IF EXISTS qbot_test')
+    await base_connection.execute('CREATE DATABASE qbot_test')
     connection = await asyncpg.connect(settings.DATABASE_URL.replace('qbot', 'qbot_test'))
-    with open(settings.BASE_DIR / 'tests/fixtures/db_schema.sql', 'r') as f:
-        await connection.execute(f.read())
+    with open(settings.BASE_DIR / 'tests/fixtures/db_schema.sql', 'r') as sql_schema_file:
+        await connection.execute(sql_schema_file.read())
         yield connection
 
     await connection.close()
-    await base_connection.execute("""DROP DATABASE qbot_test""")
+    await base_connection.execute('DROP DATABASE qbot_test')
     await base_connection.close()
 
 
 async def override_db_connection():
-    try:
+    try:  # noqa: WPS501, WPS229
         connection = await asyncpg.connect(settings.DATABASE_URL.replace('qbot', 'qbot_test'))
         yield connection
     finally:
--- src/tests/handlers/test_ayats.py
+++ src/tests/handlers/test_ayats.py
@@ -3,35 +3,37 @@ import pytest
 
 @pytest.fixture
 async def ayats(test_db_connection):
-    await test_db_connection.execute("""
+    query = """
         INSERT INTO content_sura
         (id, number, pars_hash, link, child_elements_count)
-        VALUES 
+        VALUES
         (1, 1, 'hash', 'link', 1);
-        
+
         INSERT INTO content_file
-        (id, link_to_file, tg_file_id, name) 
-        VALUES 
+        (id, link_to_file, tg_file_id, name)
+        VALUES
         (1, 'link', 'file_id', 'name');
-        
+
         INSERT INTO content_morningcontent
         (id, additional_content, day)
-        VALUES 
+        VALUES
         (1, 'content', 1);
-        
+
         INSERT INTO content_ayat
-        (additional_content, arab_text, trans, sura_id, ayat, html, audio_id, one_day_content_id, content) 
-        VALUES 
+        (additional_content, arab_text, trans, sura_id, ayat, html, audio_id, one_day_content_id, content)
+        VALUES
         ('additional', 'arab', 'transliteration', 1, '1-7', '<html></html>', 1, 1, 'content')
-    """)
+    """
+    await test_db_connection.execute(query)
 
 
 def test_get_ayats(client, db, ayats):
     got = client.get('/api/v1/ayats')
+    payload = got.json()['results']
 
     assert got.status_code == 200
     assert list(got.json().keys()) == ['count', 'next', 'prev', 'results']
-    assert list(got.json()['results'][0].keys()) == ['id', 'mailing_day']
+    assert list(payload[0].keys()) == ['id', 'mailing_day']
 
 
 def test_get_ayat_detail(client, db, ayats):
