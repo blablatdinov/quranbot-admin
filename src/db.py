bcd4318f14021ea3884c3ace3137c9cb062411f6
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-07-23 12:57:32 +0300

    create middleware

--- src/db.py
+++ src/db.py
@@ -1,9 +1,37 @@
+from typing import AsyncGenerator
+
 import asyncpg
 
 from settings import settings
 
 
-async def db_connection():
+class QueriesCountConnection(asyncpg.Connection):
+
+    def __init__(self, connection: asyncpg.Connection):
+        self._connection = connection
+        self._counter = 0
+
+    @property
+    def queries_count(self):
+        return self._counter
+
+    async def execute(self, *args):
+        self._counter += 1
+        return await self._connection.execute(*args)
+
+    async def fetch(self, *args):
+        self._counter += 1
+        return await self._connection.fetch(*args)
+
+    async def fetchrow(self, *args):
+        self._counter += 1
+        return await self._connection.fetchrow(*args)
+
+    async def close(self, *args, **kwargs) -> None:
+        return await self._connection.close(*args, **kwargs)
+
+
+async def db_connection() -> AsyncGenerator:
     """Зависимость для указания в контроллерах Fastapi.
 
     :yields: Connection
--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@ import time
 from typing import Callable
 
 import uvicorn
+from asyncpg import Connection
 from fastapi import FastAPI, Request
 from fastapi.middleware.gzip import GZipMiddleware
 
+from db import QueriesCountConnection, db_connection
 from handlers.registration_handlers import router
 from settings import settings
 
@@ -25,6 +27,24 @@ async def add_process_time_header(request: Request, call_next: Callable):
     response = await call_next(request)
     process_time = time.time() - start_time
     response.headers['X-Process-Time'] = '{0} s'.format(str(process_time))
+    return response
+
+
+@app.middleware('http')
+async def db_queries_count_middleware(request: Request, call_next: Callable):
+    """Добавляет время обработки запроса.
+
+    :param request: Request
+    :param call_next: Callable
+    :return: Response
+    """
+    connection = db_connection()
+    request.state.connection = QueriesCountConnection(
+        await connection.__anext__()
+    )
+    response = await call_next(request)
+    response.headers['X-Queries-Count'] = str(request.state.connection.queries_count)
+    await request.state.connection.close()
     return response
 
 
