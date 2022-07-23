8d5f2b1cbb23b6e03ca25e35123058a403ef1370
blablatdinov <45946541+blablatdinov@users.noreply.github.com>
2022-07-23 13:37:57 +0300

    sql queries count middleware (#9)

--- src/db.py
+++ src/db.py
@@ -1,9 +1,63 @@
+from typing import AsyncGenerator
+
 import asyncpg
 
 from settings import settings
 
 
-async def db_connection():
+class QueriesCountConnection(asyncpg.Connection):
+    """Обертка над asyncpg.Connection для подсчета кол-ва запросов."""
+
+    def __init__(self, connection: asyncpg.Connection):
+        self._connection = connection
+        self._counter = 0
+
+    @property
+    def queries_count(self):
+        """Возвращает кол-во сделанных запросов.
+
+        :return: int
+        """
+        return self._counter
+
+    async def execute(self, *args):
+        """Прокси метода.
+
+        :param args: tuple[Any]
+        :return: Any
+        """
+        self._counter += 1
+        return await self._connection.execute(*args)
+
+    async def fetch(self, *args):
+        """Прокси метода.
+
+        :param args: tuple[Any]
+        :return: Any
+        """
+        self._counter += 1
+        return await self._connection.fetch(*args)
+
+    async def fetchrow(self, *args):
+        """Прокси метода.
+
+        :param args: tuple[Any]
+        :return: Any
+        """
+        self._counter += 1
+        return await self._connection.fetchrow(*args)
+
+    async def close(self, *args, **kwargs) -> None:
+        """Прокси метода.
+
+        :param args: tuple[Any]
+        :param kwargs: dict[Any, Any]
+        :return: Any
+        """
+        return await self._connection.close(*args, **kwargs)
+
+
+async def db_connection() -> AsyncGenerator:
     """Зависимость для указания в контроллерах Fastapi.
 
     :yields: Connection
