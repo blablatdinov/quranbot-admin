25a4ea9635ac4914f128fb1443f06aed25c4e7bb
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-08-01 00:03:40 +0300

    switch to connection pool

--- src/db.py
+++ src/db.py
@@ -4,12 +4,19 @@ import asyncpg
 
 from settings import settings
 
+connection_pool = None
+
 
 async def db_connection() -> AsyncGenerator:
     """Зависимость для указания в контроллерах Fastapi.
 
     :yields: Connection
     """
-    connection = await asyncpg.connect(settings.DATABASE_URL)
-    yield connection
-    await connection.close()
+    global connection_pool  # noqa: WPS420 using for connection pool
+    if connection_pool:
+        async with connection_pool.acquire() as connection:
+            yield connection
+    else:
+        pool = await asyncpg.create_pool(settings.DATABASE_URL)  # noqa: WPS442
+        async with pool.acquire() as connection:  # type: ignore # noqa: WPS440
+            yield connection
