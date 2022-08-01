e7423f9192c04fe43c7340c4d743505076ae6533
blablatdinov <45946541+blablatdinov@users.noreply.github.com>
2022-08-01 19:45:36 +0300

    Refactor router prefix (#24)

--- src/handlers/registration_handlers.py
+++ src/handlers/registration_handlers.py
@@ -11,10 +11,10 @@ from handlers.v1.views.ayats import router as ayats_router
 from handlers.v1.views.mailings import router as mailings_router
 from handlers.v1.views.messages import router as messages_router
 
-router = APIRouter()
+router = APIRouter(prefix='/api/v1')
 
-router.include_router(messages_router, prefix='/api/v1', tags=['Messages'])
-router.include_router(mailings_router, prefix='/api/v1', tags=['Mailings'])
-router.include_router(ayats_router, prefix='/api/v1', tags=['Ayats'])
-router.include_router(daily_content_router, prefix='/api/v1', tags=['Daily content'])
-router.include_router(auth_router, prefix='/api/v1', tags=['Auth'])
+router.include_router(messages_router, tags=['Messages'])
+router.include_router(mailings_router, tags=['Mailings'])
+router.include_router(ayats_router, tags=['Ayats'])
+router.include_router(daily_content_router, tags=['Daily content'])
+router.include_router(auth_router, tags=['Auth'])
