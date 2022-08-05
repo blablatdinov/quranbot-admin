d69d7ab3ae1c462b500cd98b5f642011d30ec3d7
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-08-06 02:51:14 +0300

    test endpoint

--- src/tests/handlers/test_messages_graph.py
+++ src/tests/handlers/test_messages_graph.py
@@ -1,10 +1,21 @@
+import datetime
+
+import pytest
+
 from main import app
+from repositories.messages import MessageRepository, MessageRepositoryInterface
+from handlers.v1.schemas.messages import MessageGraphDataItem
 
 
+class MessageRepositoryMock(MessageRepositoryInterface):
+    
+    async def get_messages_for_graph(self, start_date, finish_date):
+        return [MessageGraphDataItem(date=datetime.date(2022, 8, 6), messages_count=1)]
 
 
+@pytest.fixture(autouse=True)
 def override_dependency():
-    app.dependency_overrides[]
+    app.dependency_overrides[MessageRepository] = MessageRepositoryMock
 
 
 def test(client):
