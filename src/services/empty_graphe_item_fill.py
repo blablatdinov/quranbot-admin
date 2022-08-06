e185830aa0df47c51f77bc13a4636fa1c7234382
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-08-06 03:37:00 +0300

    simple decorator

--- src/services/empty_graphe_item_fill.py
+++ src/services/empty_graphe_item_fill.py
@@ -1,5 +1,12 @@
-class GraphZeroValueItemSave(object):
-    pass
+import datetime
 
-    #def __init__(self, graph_data: list):
-        #self._grap
+from repositories.messages import MessageRepositoryInterface
+
+
+class GraphZeroValueItemSave(object):
+
+    def __init__(self, message_repository: MessageRepositoryInterface):
+        self._message_repository = message_repository
+
+    async def get_messages_for_graph(self, start_date: datetime.date, finish_date: datetime.date):
+        return await self._message_repository.get_messages_for_graph(start_date, finish_date)
--- src/tests/test_graph_zero_value_save.py
+++ src/tests/test_graph_zero_value_save.py
@@ -1,5 +1,18 @@
+import datetime
+
 from services.empty_graphe_item_fill import GraphZeroValueItemSave
+from handlers.v1.schemas.messages import MessageGraphDataItem
+from repositories.messages import MessageRepositoryInterface
 
 
-def test():
-    got = GraphZeroValueItemSave()
+class MessageRepositoryMock(MessageRepositoryInterface):
+
+    async def get_messages_for_graph(self, start_date, finish_date):
+        return [MessageGraphDataItem(date=datetime.date(2022, 8, 6), messages_count=1)]
+
+
+async def test():
+    got = await GraphZeroValueItemSave(MessageRepositoryMock()).get_messages_for_graph(
+        datetime.date(2022, 7, 6),
+        datetime.date(2022, 8, 6),
+    )
