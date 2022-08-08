25d5a10fa9c2c1ec2306e50723b494c39d98bfb4
blablatdinov <45946541+blablatdinov@users.noreply.github.com>
2022-08-08 21:28:17 +0300

    Notifications (#48)

--- src/handlers/v1/schemas/notifications.py
+++ src/handlers/v1/schemas/notifications.py
@@ -2,11 +2,21 @@
 
 Classes:
     NotificationCreateModel
+    NotificationResponseSchema
 """
+import uuid
+
 from pydantic import BaseModel
 
 
 class NotificationCreateModel(BaseModel):
     """Модель для создания уведомлений."""
 
+    text: str
+
+
+class NotificationResponseSchema(BaseModel):
+    """Схема ответа для просмотра уведомлений."""
+
+    uuid: uuid.UUID
     text: str
