5e4d779ea81cca451bef2dc51eae70a202f3671a
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2023-09-01 19:13:45 +0300

    Deploy action

---
+++ migrations/20230902_01_QkBQH-init-schema.rollback.sql
@@ -0,0 +1,16 @@
+-- Init schema
+-- depends:
+
+DROP TABLE users CASCADE;
+
+DROP TABLE ayats;
+
+DROP TABLE cities;
+
+DROP TABLE files;
+
+DROP TABLE messages;
+
+DROP TABLE suras;
+
+DROP TABLE user_actions;
