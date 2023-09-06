fff31b78e36349c4030f7bd7968da48c1a71fe37
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2023-09-06 19:16:43 +0300

    Stub websocket

--- src/handlers/websockets.py
+++ src/handlers/websockets.py
@@ -5,13 +5,10 @@ Functions:
 """
 import asyncio
 
-import nats
 from aioredis.client import Redis
 from fastapi import Depends, Query, WebSocket
 
 from caching import redis_connection
-from services.auth import User
-from services.websocket_user import WebsocketUser
 
 
 async def websocket_endpoint(
@@ -25,10 +22,6 @@ async def websocket_endpoint(
     :param token: str
     :param redis: Redis
     """
-    user = User.get_from_token(token)
     await websocket.accept()
-    nats_client = await nats.connect('localhost')
-    await redis.set('websocket_client_{0}'.format(user.username), 'true')
-    await nats_client.subscribe('default', cb=WebsocketUser(websocket, user).receive)
     while True:  # noqa: WPS457
         await asyncio.sleep(1)
