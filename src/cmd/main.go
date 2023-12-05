3c818ca5f2471b3abcfeeb8e5f6e033e83fc40cd
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2023-12-05 08:56:09 +0300

    Simple server

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func health(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "ok")
+}
 
 func main() {
-	fmt.Println("Hello")
+	http.HandleFunc("/health", health)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
