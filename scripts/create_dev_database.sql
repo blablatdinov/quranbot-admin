ca8da4c2d2c227f118f063d149bfae07140de289
Ilaletdinov Almaz <45946541+blablatdinov@users.noreply.github.com>
2024-02-18 14:00:03 +0300

    Django setup [skip ci] (#297)

---
+++ scripts/create_dev_database.sql
@@ -0,0 +1,8 @@
+/*
+This file is used to bootstrap development database locally.
+
+Note: ONLY development database;
+*/
+
+CREATE USER "quranbot-admin" SUPERUSER;
+CREATE DATABASE "quranbot-admin" OWNER "quranbot-admin" ENCODING 'utf-8';
