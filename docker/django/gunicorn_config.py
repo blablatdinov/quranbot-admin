bfbd3626cbb99cfbb2f37253a8f54123bd978503
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2024-02-20 21:36:18 +0300

    Deployed on server

--- docker/django/gunicorn_config.py
+++ docker/django/gunicorn_config.py
@@ -6,7 +6,7 @@ https://docs.gunicorn.org/en/stable/settings.html
 
 import multiprocessing
 
-bind = '0.0.0.0:8000'
+bind = '0.0.0.0:8010'
 # Concerning `workers` setting see:
 # https://github.com/wemake-services/wemake-django-template/issues/1022
 workers = multiprocessing.cpu_count() * 2 + 1
