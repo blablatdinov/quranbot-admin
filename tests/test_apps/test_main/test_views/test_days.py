416e8df94f75928de97956828cbce88fc725fd9a
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2024-02-24 00:29:57 +0300

    Form for generating ayat days

---
+++ tests/test_apps/test_main/test_views/test_days.py
@@ -0,0 +1,39 @@
+import pytest
+from bs4 import BeautifulSoup
+
+from server.apps.main.models import Ayat
+
+pytestmark = [pytest.mark.django_db]
+
+
+@pytest.fixture()
+def nullday_ayats(mixer):
+    return mixer.cycle(8).blend('main.Ayat', day=None)
+
+
+@pytest.fixture()
+def ayats(mixer):
+    return mixer.cycle(20).blend('main.Ayat')
+
+
+def test_get(client, ayats, nullday_ayats):
+    response = client.get('/days')
+
+    assert response.status_code == 200
+    assert [f'ayat-{x.ayat_id}' for x in nullday_ayats] == [
+        input_['name']
+        for input_ in BeautifulSoup(response.content.decode('utf-8')).find_all('input', class_='form-check-input')
+    ]
+
+
+def test_post(client, nullday_ayats):
+    post_data = {'day': 14} | {f'ayat-{ayat.ayat_id}': '' for ayat in nullday_ayats[:4]}
+    response = client.post(
+        '/days',
+        post_data,
+    )
+
+    assert response.status_code == 200
+    assert set(
+        Ayat.objects.filter(ayat_id__in=[a.ayat_id for a in nullday_ayats[:4]]).values_list('day', flat=True),
+    ) == {14}
